8834975f067cd7b1797b22c5ae44cdc4a736fc92
mueen <moenali159@gmail.com>
2019-08-18 02:25:46 +0800

    created the auth service with the login and register methods and injected it in register component and login component

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+
 import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent, NavComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
